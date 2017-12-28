a3d951651d1633cde947771a05ecddb091c30da6
Ignacio Murillo <ignaciomurillo97@gmail.com>
2017-12-28 17:39:43 -0600

    -- Script de python que pide los datos del viento de la api

---
+++ requester/req.py
@@ -0,0 +1,54 @@
+#!/bin/python3
+import requests
+import sys
+import json
+from time import *
+
+latSamples = 10
+lonSamples = 10
+
+sampleDict = {}
+
+def main():
+    getSamples()
+    print(sampleDict)
+
+def getSamples():
+    latStep = 180 / latSamples
+    lonStep = 360 / lonSamples
+    maxSamples = latSamples * lonSamples
+    for i in range(latSamples):
+        for j in range(lonSamples):
+            lat = j * latStep - 90
+            lon = i * lonStep - 180
+            windData = getWindAt(lat, lon)
+            if (windData):
+                sampleDict[(lat, lon)] = windData
+            else:
+                print("failed at: " + str((lat, lon)))
+            sleep(1)
+            progress(i * latSamples + j, maxSamples)
+
+def getWindAt(lat, lon):
+    r = requests.get(buildUrl(lat, lon))
+    resData = json.loads(r.text)
+    if(resData["cod"] == 200):
+        windData = resData["wind"]
+        return resData["wind"]
+
+def buildUrl(lat, lon):
+    url = "http://api.openweathermap.org/data/2.5/weather?lat="
+    url += str(lat )+"&lon="+ str(lon)
+    url += "&APPID=d5269ad59d408c881bd47821a970ab87"
+    return url 
+
+def progress(count, total, status=''):
+    barLen = 60
+    filledLen = int(round(barLen * count / float(total)))
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filledLen + '-' * (barLen - filledLen)
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
+
+if (__name__ == '__main__'):
+    main();
